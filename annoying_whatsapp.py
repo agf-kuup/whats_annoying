e93b89affc1693537695ea19f25c0048aa3f771e
coder.generico <agf0710@gmail.com>
2020-06-17 18:42:42 -0500

    Deleted not used libraries and better spaced

--- annoying_whatsapp.py
+++ annoying_whatsapp.py
@@ -1,9 +1,6 @@
-from dataclasses import dataclass
 from selenium import webdriver
-from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.keys import Keys
 from itertools import cycle
-import os
 import time # Just to wait a bit between messages.
 
 
@@ -12,8 +9,10 @@ name_victim="Vale Chay"
 looping=False
 
 def infinitize_text(loop):
-    '''Generates a cycle or a list of messages 
-    depending on the variable looping.'''
+    '''
+    Generates a cycle or a list of messages 
+    depending on the variable looping.
+    '''
 
     if not loop:
         messages=[l for l in open('content.txt','r+')]
@@ -25,10 +24,13 @@ def search_chat(driv,victim):
     '''
     Looks for the search box in the chat window.
     '''
+
     path_to_search='//*[@id="side"]/div[1]/div/label/div/div[2]'
+    
     searcher=driv.find_element_by_xpath(path_to_search)
     searcher.send_keys(victim)
     time.sleep(1) # Gives time the browser updates the search of the victim
+    
     pray_found='//*[@id="pane-side"]/div[1]/div/div/div[2]/div/div'
     pray=driv.find_element_by_xpath(pray_found)
     pray.click()
@@ -61,8 +63,10 @@ def start_annoying(driv,loop):
 def start(_victim,_driver,_loop):
     driver = webdriver.Chrome(executable_path=r'{}'.format(_driver))
     driver.get("https://web.whatsapp.com/")
+    
     input("Click when QR is done")
     time.sleep(5)
+    
     search_chat(driver,_victim)
     start_annoying(driver,_loop)
     driver.close()
