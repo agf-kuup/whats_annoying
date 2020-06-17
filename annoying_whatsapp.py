ec6f71f90ed5d7065bc4b9075f762a68de5bc896
coder.generico <agf0710@gmail.com>
2020-06-17 18:29:55 -0500

    Surrended. Not classes, just functions

--- annoying_whatsapp.py
+++ annoying_whatsapp.py
@@ -7,61 +7,65 @@ import os
 import time # Just to wait a bit between messages.
 
 
-@dataclass
-class annoy:
-    victim: str                         # Name of the chat you will look for.
-    size: int=None                      # Number of messages you want to send.
-    loop: bool=False                    # The content will be repeated forever.
-    if loop:
-        messages=cycle([l for l in open('content.txt','r+')])
-    else:
+driver_path='chrome_driver/chromedriver.exe'
+name_victim="Vale Chay"
+looping=False
+
+def infinitize_text(loop):
+    '''Generates a cycle or a list of messages 
+    depending on the variable looping.'''
+
+    if not loop:
         messages=[l for l in open('content.txt','r+')]
+    else:
+        messages=cycle([l for l in open('content.txt','r+')])
+    return messages
 
-    @staticmethod
-    def search_chat(driv,victim):
-        path_to_search='//*[@id="side"]/div[1]/div/label/div/div[2]'
-        searcher=driv.find_element_by_xpath(path_to_search)
-        searcher.send_keys(victim)
-        time.sleep(1) # Gives time the browser updates the search of the victim
-        pray_found='//*[@id="pane-side"]/div[1]/div/div/div[2]/div/div'
-        pray=driv.find_element_by_xpath(pray_found)
-        pray.click()
+def search_chat(driv,victim):
+    '''
+    Looks for the search box in the chat window.
+    '''
+    path_to_search='//*[@id="side"]/div[1]/div/label/div/div[2]'
+    searcher=driv.find_element_by_xpath(path_to_search)
+    searcher.send_keys(victim)
+    time.sleep(1) # Gives time the browser updates the search of the victim
+    pray_found='//*[@id="pane-side"]/div[1]/div/div/div[2]/div/div'
+    pray=driv.find_element_by_xpath(pray_found)
+    pray.click()
 
-    @staticmethod
-    def send_msg(mbox):
-        if annoy.loop:
-            mbox.send_keys(Keys.ENTER)
-            time.sleep(1)
-            mbox.send_keys(next(annoy.messages))
-            time.sleep(1)
-            mbox.send_keys(Keys.ENTER)
-        else:
-            mbox.send_keys(Keys.ENTER)
-            time.sleep(1)
-            mbox.send_keys(self.messages.pop(0))
-            time.sleep(1)
-            mbox.send_keys(Keys.ENTER)
+def send_msg(mbox,msgs,loop):
+    '''
+    Writes the next message in the input box and sends it.
+    '''    
+    if loop:
+        mbox.send_keys(next(msgs))
+    else:
+        mbox.send_keys(msgs.pop(0))
+    
+    time.sleep(1)
+    mbox.send_keys(Keys.ENTER)
 
-    @staticmethod
-    def start_annoying(driv,msgs,size):
-        i=size if size else -1 
+def start_annoying(driv,loop):
+    path_message_box='//*[@id="main"]/footer/div[1]/div[2]'
+    messages=infinitize_text(loop)
+
+    i=len(messages) if type(messages)==list else -1
+    
+    messageBox=driv.find_element_by_xpath(path_message_box)
+    messageBox.click()
         
-        messageBox=driv.find_element_by_class_name("_1Plpp")
-        messageBox.click()
+    while i:    
+        send_msg(messageBox,messages,loop)
+        i-=1
 
-        while i:    
-            self.send_msg(messageBox)
-            i-=1
+def start(_victim,_driver,_loop):
+    driver = webdriver.Chrome(executable_path=r'{}'.format(_driver))
+    driver.get("https://web.whatsapp.com/")
+    input("Click when QR is done")
+    time.sleep(5)
+    search_chat(driver,_victim)
+    start_annoying(driver,_loop)
+    driver.close()
 
-    @property
-    def run(self):
-        driver = webdriver.Chrome(executable_path=r'chrome_driver/chromedriver.exe')
-        driver.get("https://web.whatsapp.com/")
-        input("Click when QR is done")
-        time.sleep(5)
-        self.search_chat(driver,self.victim)
-        self.start_annoying(driver,self.messages,self.size)
-        driver.close()
 
-A=annoy(victim="Vale Chay")
-A.run
+start(name_victim,driver_path,looping)
